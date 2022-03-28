f4785f686d2d2751a532857353d6b57fbb342647
Yee Kit <yeek3063@gmail.com>
2022-03-28 14:31:02 +0800

    Add files via upload

---
+++ Q3a_swtich.py
@@ -0,0 +1,16 @@
+currency = "CSC1009"
+
+if currency == "CSC1009":
+    print("Object-Oriented Programming")
+elif currency ==  "CSC1006":
+    print("Mathematics 2")
+elif currency ==  "CSC1010":
+    print("Computer Networks")
+elif currency ==  "CSC1007":
+    print("Operating Systems")
+elif currency ==  "CSC2902":
+    print("Career and Professional Development")
+elif currency ==  "1008":
+    print("Data Structures and Algorithms")
+else :
+    print("Unknown")
